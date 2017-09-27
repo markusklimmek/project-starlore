4f34c9090ae569153d8115a5215d8a119f9a239a
Markus Klimmek <mklimmek@gmx.de>
2017-09-27 22:01:12 +0200

    we need a fine classification scheme

--- starlore/src/main/java/com/justcoffee/projects/core/PlanetClassification.java
+++ starlore/src/main/java/com/justcoffee/projects/core/PlanetClassification.java
@@ -1,12 +1,13 @@
 package com.justcoffee.projects.core;
-
+// TODO: We need a clever classification system, there is perhaps one in astronomy or we take one from Star Trek
 public enum PlanetClassification {
     EARTHLIKE,
     MARTIAN;
 
     private static PlanetClassification classification;
-
+    //TODO: it should be truly random :)
+    //TODO: how to test for wanted randomness???
     public static PlanetClassification getRandomClassification() {
-        return EARTHLIKE;
+        return MARTIAN;
     }
 }
